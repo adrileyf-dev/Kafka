310ac61fbf3abd292e4746568280e4eb820bdb33
adriley francisco almeida pereira <adrileyfdev@gmail.com>
2024-04-29 19:30:24 -0300

    inicio projeto kafka

---
+++ src/main/java/dev/com/kafkadev/KafkadevApplication.java
@@ -0,0 +1,13 @@
+package dev.com.kafkadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkadevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkadevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/com/kafkadev/KafkadevApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.com.kafkadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkadevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
